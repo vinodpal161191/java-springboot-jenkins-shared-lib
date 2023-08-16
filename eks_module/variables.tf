7786040dea03e24cd8d22c4dc5265cebf8d19662
Vinod Pal <vinod161191@gmail.com>
2023-08-16 18:19:37 +0530

    jenkins file for deploy springboot app to eks

---
+++ eks_module/variables.tf
@@ -0,0 +1,42 @@
+
+variable "access_key" {
+    default = ""
+}
+variable "secret_key" {
+    default = ""
+}
+
+
+variable "region" {
+    default = "ap-south-1"
+}
+
+variable "aws_eks_cluster_config" {
+  
+}
+variable "eks_node_group_config" {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
